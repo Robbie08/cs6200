f9cc30a6707d408f9fafacf02953a66620a04c8c
rortiz38 <roberto.ortiz@gatech.edu>
2025-01-19 14:23:47 -0800

    Simplified client and server logic, all tests passing

--- pr1/transfer/transferclient.c
+++ pr1/transfer/transferclient.c
@@ -8,8 +8,11 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 
 #define BUFSIZE 512
+#define NETWORK_BUFF_SIZE 1024
+#define MAX_PORT_SIZE 6
 
 #define USAGE                                                \
   "usage:\n"                                                 \
@@ -27,6 +30,10 @@ static struct option gLongOptions[] = {
     {"help", no_argument, NULL, 'h'},
     {"port", required_argument, NULL, 'p'},
     {NULL, 0, NULL, 0}};
+
+// Function prototype declarations
+int createSocketAndConnect(struct addrinfo *addressesList);
+void saveFileSentByServer(int sockfd, int fileFd);
 
 /* Main ========================================================= */
 int main(int argc, char **argv)
@@ -76,4 +83,102 @@ int main(int argc, char **argv)
     }
 
     /* Socket Code Here */
+    struct addrinfo addressConfig;
+    
+    // Zero out and set up our address config
+    memset(&addressConfig, 0, sizeof addressConfig);
+    addressConfig.ai_family = AF_UNSPEC;
+    addressConfig.ai_socktype = SOCK_STREAM;
+
+    char portNoStr[MAX_PORT_SIZE];
+    memset(&portNoStr, 0, sizeof portNoStr);
+    sprintf(portNoStr, "%d", portno);
+
+    int status;
+    struct addrinfo *addressesList;
+
+    status = getaddrinfo(hostname, portNoStr, &addressConfig, &addressesList);
+    if (status == -1) {
+        perror("client: getaddrinfo");
+        exit(1);
+    }
+
+    int sockfd;
+    sockfd = createSocketAndConnect(addressesList);
+
+    // We are connected so the server will start sending us the file
+    // let's create a file and start appending the contents that were sent
+    int fileFd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fileFd == -1) {
+        perror("client: open");
+        close(sockfd);
+        return 1;
+    }
+
+    saveFileSentByServer(sockfd, fileFd);
+
+    close(fileFd);
+    close(sockfd);
+    return 0;
+}
+
+// This method creates a socket and connects with the first available server address, provided by the addressesList(linked list).
+// Returns the socket's file descriptor if successfully connected, otherwise terminates the program.
+int createSocketAndConnect(struct addrinfo *addressesList) {
+    int sockfd;
+    int err; 
+    struct addrinfo *curr;
+
+    // iterate over linked list until we find a connection
+    for (curr = addressesList; curr != NULL; curr = curr->ai_next) {
+        sockfd = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+        if (sockfd == -1){
+            perror("client: socket");
+            continue;
+        }
+
+        err = connect(sockfd, curr->ai_addr, curr->ai_addrlen);
+        if (err == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break; // we've connected another machine through the socket
+    }
+
+    if (curr == NULL) {
+        perror("client: failed to connect");
+        freeaddrinfo(addressesList);
+        exit(2);
+    }
+
+    freeaddrinfo(addressesList); // we don't need the linked list anymore, so let's free it up
+    return sockfd;
+}
+
+// This method reads the file sent by the server and saves it to the specified file. It's possible that
+// the file is too large to be sent in one network transaction, so we need to keep reading from the socket,
+// appending the contents into the file until the server closes the connection.
+void saveFileSentByServer(int sockfd, int fileFd) {
+    char buff[NETWORK_BUFF_SIZE];
+    memset(&buff, 0, NETWORK_BUFF_SIZE);
+
+    ssize_t bytesRecvd, bytesWritten;
+
+    // Read contents until server closes connection (until recv returns 0)
+    while((bytesRecvd = recv(sockfd, buff, NETWORK_BUFF_SIZE, 0)) > 0) {
+        // write the recvd data into the file
+        bytesWritten = write(fileFd, buff, bytesRecvd);
+        if (bytesWritten == -1) {
+            perror("client: write");
+            close(fileFd);
+            close(sockfd);
+            exit(1);
+        }
+    }
+    
+    if (bytesRecvd == -1) {
+        perror("client: recv");
+    }
 }
--- pr1/transfer/transferserver.c
+++ pr1/transfer/transferserver.c
@@ -8,8 +8,11 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <fcntl.h>
 
 #define BUFSIZE 512
+#define NETWORK_BUFF_SIZE 1024
+#define MAX_PORT_SIZE 6
 
 #define USAGE                                                \
     "usage:\n"                                               \
@@ -26,11 +29,15 @@ static struct option gLongOptions[] = {
     {"port", required_argument, NULL, 'p'},
     {NULL, 0, NULL, 0}};
 
+int createAndBindSocket(struct addrinfo *adressesList);
+void sendFileContents(int filefd, int connectionFd);
+
 int main(int argc, char **argv)
 {
     int option_char;
     char *filename = "6200.txt"; /* file to transfer */
     int portno = 23948;             /* port to listen on */
+    int maxnpending = 5;
 
     setbuf(stdout, NULL); // disable buffering
 
@@ -62,8 +69,151 @@ int main(int argc, char **argv)
     if (NULL == filename) {
         fprintf(stderr, "%s @ %d: invalid filename\n", __FILE__, __LINE__);
         exit(1);
-    }
+    }    
 
     /* Socket Code Here */
+    struct addrinfo addrConfig;
 
+    // We want to ensure the struct is zero'd out and empty
+    memset(&addrConfig, 0, sizeof addrConfig);
+    addrConfig.ai_family = AF_UNSPEC; // to allow both IPv4 and IPv6
+    addrConfig.ai_socktype = SOCK_STREAM; // Since we want to make this a TCP socket
+    addrConfig.ai_flags = AI_PASSIVE; // Tells getaddrinfo() to assign local host to the socket structures
+
+    // convert port that's an int to string 
+    char portNoStr[MAX_PORT_SIZE];
+    memset(&portNoStr, 0, sizeof portNoStr);
+    sprintf(portNoStr, "%d", portno);
+
+    int status; // holds status that we use to check for errs
+    struct addrinfo *adressesList; // points to the linked list containing the socket addresses resolved by getaddrinfo
+
+    status = getaddrinfo(NULL, portNoStr, &addrConfig, &adressesList);
+    if (status != 0) {
+        // Send error to stderr and stop the program since ther's no point to continue if getaddrinfo fails
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    // At this point the adressesList now points to the linked list of 1 or more struct addrinfos
+    int sockfd;
+    sockfd = createAndBindSocket(adressesList);
+
+    int err = 0;
+    err = listen(sockfd, maxnpending);
+    if (err == -1) {
+        perror("server: listen");
+        close(sockfd);
+        exit(1);
+    }
+
+    int filefd = open(filename, O_RDONLY);
+    if (filefd == -1) {
+        perror("server: open");
+        close(sockfd);
+        exit(1);
+    }
+
+    int newConFd;
+    socklen_t addrSize;
+    struct sockaddr_storage connectorsAddress; // usng sockaddr_storage since its big enough to hold IPv4 and IPv6 addresses
+
+    for(;;) {
+        addrSize = sizeof connectorsAddress;
+
+        // Attempt to connect with the connection (blocking call)
+        newConFd = accept(sockfd, (struct sockaddr *)&connectorsAddress, &addrSize);
+        if (newConFd == -1) {
+            perror("server: accept");
+            continue;
+        }
+
+        // Now that we're connected, let's send the contents of the file
+        sendFileContents(filefd, newConFd);
+        
+        close(newConFd);
+        lseek(filefd, 0, SEEK_SET); // reset the filefd to the start of the file so that we can read from the start the next time around
+    }
+
+    close(filefd);
+    close(sockfd);
+    return 0;
+}
+
+
+// This function creates a socket and binds to the first valid address in the addressList (linked list).
+// The socket's file descriptor is retured if the operation succeeded.
+int createAndBindSocket(struct addrinfo *adressesList) {
+    int sockfd;
+    struct addrinfo *curr;
+    int yes = 1;
+    int err;
+    for (curr = adressesList; curr != NULL; curr = curr->ai_next) {
+
+        // Attempt to create a socket until success
+        sockfd = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+        if (sockfd == -1) {
+            perror("server: socket");
+            continue;
+        }
+        
+        err = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        // If our port is still in use then lets just force it by allowing our program to use reuse it
+        if(err == -1) {
+            perror("server: setsockopt");
+            exit(1); // No point in continuing if for some reason we can't reuse the port since subsequent code will fail
+        }
+        
+        err = bind(sockfd, curr->ai_addr, curr->ai_addrlen);
+        if (err == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue; // Just becuase this one failed to bind doesn't mean there isn't another one available
+        }
+
+        break; // if we made it this far then we've created a socket and associated it with a port number on our machine.
+    }
+
+    // Once we're done with adressesList let's free up the linked list
+    freeaddrinfo(adressesList);
+
+    if (curr == NULL) {
+        fprintf(stderr, "server: failed to bind");
+        close(sockfd);
+        exit(1);
+    }
+
+    return sockfd;
+}
+
+
+// This method writes the file's content based on the file's file descriptor and writes them into the
+// connection's file descriptor in chunks. It's possible we cannot fit all the contents of the file
+// in one network transaction so we need keep sending chunks until we've sent all the file's contents.
+void sendFileContents(int filefd, int connectionFd) {
+    char buff[NETWORK_BUFF_SIZE];
+    memset(&buff, 0, NETWORK_BUFF_SIZE);
+    size_t bytesRead, bytesSent;
+    while ((bytesRead = read(filefd, buff, sizeof(buff))) > 0) {
+        char *bufPtr = buff; // Allows me to keep track of the next chunk of bytes I need to send
+        ssize_t bytesToSend = bytesRead;
+        while(bytesToSend > 0) {
+            bytesSent = send(connectionFd, bufPtr, bytesToSend, 0);
+            if (bytesSent == -1) {
+                perror("server: send");
+                close(connectionFd);
+                continue;
+            } else if (bytesSent == 0) {
+                perror("server: send failed because the client closed the connection.");
+                close(connectionFd);
+                continue;
+            }
+            bytesToSend -= bytesSent; // subtract the bytes we sent from the total bytes we need to send
+            bufPtr += bytesSent; // move our bufPtr to the start of next chunk
+        }
+    }
+
+    if (bytesRead == -1) {
+        perror("server: send");
+    }
 }
