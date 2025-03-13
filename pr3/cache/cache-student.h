09064af40cb9ac57afb3a45dcd8c652b270d5776
rortiz38 <roberto.ortiz@gatech.edu>
2025-03-12 21:02:52 -0700

    Added logic to the handle_with_cache.c to fetch file from server if the cache doesn't have it. Added TODO comments for cache portion that needs implementing.

--- pr3/cache/cache-student.h
+++ pr3/cache/cache-student.h
@@ -6,4 +6,25 @@
  #define __CACHE_STUDENT_H__844
 
  #include "steque.h"
+ #include <stddef.h>
+ #include <curl/curl.h> 
+
+ /**
+ * Structure to hold the buffer and its size. This struct gets passed into the write_callback
+ * function as the userdata parameter. The write_callback function will then use this struct
+ * to store the buffer and its size.
+ * 
+ * Credit: I got a good portion of this code from the example code in the curl documentation:
+ * https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+ */
+typedef struct {
+	char *data; 	// ptr to the dynamically allocated buffer for response data
+	size_t size; 	// current buff suize
+} BuffStruct; 
+
+size_t write_callback(void *data_ptr, size_t size, size_t nmemb, void *userdata);
+
+char *get_full_url(const char *path, const char *server);
+
+void cleanup(CURL *curl, char **full_path, BuffStruct *bufferStruct);
  #endif // __CACHE_STUDENT_H__844
--- pr3/cache/handle_with_cache.c
+++ pr3/cache/handle_with_cache.c
@@ -3,12 +3,156 @@
 
 #define BUFSIZE (834)
 
+ssize_t handle_with_cache(gfcontext_t *ctx, const char *path, void* arg) {
+	(void) ctx;
+	const char *server = (const char *)arg;
+	(void) path;
+	errno = ENOSYS;
+
+	// TODO: Make a cache request asking if it has the file
+	// TODO: If it has the file then just respond back to the client
+
+	// If the file isn't found on the cache then request it from the server
+	CURL *curl = curl_easy_init(); 
+	if (curl == NULL) {
+		perror("server: curl_easy_init");
+		return -1;
+	}
+
+	char *full_path = get_full_url(path, server);
+	if (full_path == NULL) {
+		perror("server: get_full_url failed");
+		curl_easy_cleanup(curl);
+		return -1;
+	}
+
+	printf("server: full_path: %s\n", full_path);
+	curl_easy_setopt(curl, CURLOPT_URL, (const char *)full_path); // must define the path otherwise no trasnfer will occur
+	curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT_MS, 5000L); // timeout after 5 seconds if can't perform successful TCP handshake
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // Follow redirects if necessary using the ALL flag
+	curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5L); // Allow up to 5 redirects to avoid infinite loops or long chain of redirects
+	curl_easy_setopt(curl, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTPS); // Only allow redirects to HTTPS servers
+	
+	// Let's just create our own callback function for writing data
+	BuffStruct bufferStruct = {NULL, 0};
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &bufferStruct); // Allows the write_callback to write to the BufferStruct
+	
+	CURLcode res;
+	res = curl_easy_perform(curl);
+	if (res != CURLE_OK) {
+		// clean the allocated memory
+		fprintf(stderr, "server: curl_easy_perform returned unexpected error: %s\n", curl_easy_strerror(res));
+		gfs_sendheader(ctx, GF_ERROR, 0);
+		cleanup(curl, &full_path, &bufferStruct);
+		return -1;
+	}
+
+	// Lets get the http code from the response
+	long http_code = 0;
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+	if (http_code == 404 || http_code == 403) {
+		// If we couldn't find the file then let's send a 404 error to the client
+		fprintf(stderr, "server: curl_easy_perform returned 404 or 403 error... Responding to client with 'GF_FILE_NOT_FOUND' status.\n");
+		gfs_sendheader(ctx, GF_FILE_NOT_FOUND, 0);
+		cleanup(curl, &full_path, &bufferStruct);
+		return -1;
+	} else if (http_code >= 400) {
+		// For any other error 4xx and 5xx errors lets return a GF_ERROR
+		fprintf(stderr, "server: curl_easy_perform returned the error code: %ld\n", http_code);
+		gfs_sendheader(ctx, GF_ERROR, 0);
+		cleanup(curl, &full_path, &bufferStruct);
+		return -1;
+	}
+
+	// Send the GETFILE response
+	printf("server: responding to client with 'GF_OK' status\n");
+	// If we get here then we have a successful response so just return GF_OK header and then data
+	gfs_sendheader(ctx, GF_OK, bufferStruct.size); // Send the buffer size to the client
+	gfs_send(ctx, bufferStruct.data, bufferStruct.size); // Send the actual data to the client
+
+
+	// TODO: Send the file to the cache. If the cache doesn't save the file because its full 
+	// or it runs into some other unknown error then it's ok I guess. Worst case we just
+	// request the file from the server again.
+
+	// clean the allocated memory
+	size_t total_size = bufferStruct.size;
+	cleanup(curl, &full_path, &bufferStruct);
+	return total_size;	// need to return the file size here
+}
+
+/**
+ * Callback function for writing data from the curl request and return the
+ * total size of the data written. This function matches the prototype of
+ * the CURLOPT_WRITEFUNCTION option for curl_easy_setopt and uses the userdata
+ * parameter to store the buffer and its size as specified by the setopt function.
+ * 
+ * Credit: I got a good portion of this code from the example function cb in the
+ * curl documentation: https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html
+ */
+size_t write_callback(void *data_ptr, size_t size, size_t nmemb, void *userdata) {
+    size_t total_size = size * nmemb; // Calculate the actual size that will be written
+    BuffStruct *buff = (BuffStruct *)userdata;
+
+    char *temp_buff = realloc(buff->data, buff->size + total_size + 1); // Allocate or reallocate memory for the buffer
+    if (temp_buff == NULL) {
+        fprintf(stderr, "server: realloc failed to allocate memory\n");
+        return 0; // Return 0 to signal an error
+    }
+
+    buff->data = temp_buff; // Update the buffer pointer
+    memcpy(&buff->data[buff->size], data_ptr, total_size); // Copy the new data to the buffer
+    buff->size += total_size; // Update the buffer size
+    buff->data[buff->size] = '\0'; // Null-terminate the buffer
+
+    return total_size;
+}
+
+/**
+ * This function will concatenate the base url with the path to the file to get the full url
+ * to the file. This function allocates memory for the full url.
+ */
+char *get_full_url(const char *path, const char *server) {
+    size_t url_len = strlen(server) + strlen(path) + 1;
+    char *url = malloc(url_len);
+    if (url == NULL) {
+        perror("server: malloc failed to allocate memory");
+        return NULL;
+    }
+
+    // The following will concatenate the base_url and the path
+    snprintf(url, url_len, "%s%s", server, path);
+    return url;
+}
+
+/**
+ * This function will clean up the allocated memory for the full path and the buffer struct
+ * and call the curl_easy_cleanup function to clean up the curl handle.
+ */
+void cleanup(CURL *curl, char **full_path, BuffStruct *bufferStruct) {
+    if (*full_path != NULL) {
+        free(*full_path);
+        *full_path = NULL;
+    }
+
+    if (bufferStruct->data != NULL) {
+        free(bufferStruct->data);
+        bufferStruct->data = NULL;
+    }
+
+    if (curl != NULL) {
+        curl_easy_cleanup(curl);
+    }
+}
+
 /*
  __.__
 Replace with your implementation
  __.__
 */
-ssize_t handle_with_cache(gfcontext_t *ctx, const char *path, void* arg){
+ssize_t handle_with_cache_old(gfcontext_t *ctx, const char *path, void* arg){
 	size_t file_len;
     size_t bytes_transferred;
 	char *data_dir = arg;
@@ -60,4 +204,3 @@ ssize_t handle_with_cache(gfcontext_t *ctx, const char *path, void* arg){
 
 
 }
-
